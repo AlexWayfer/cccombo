956d8449a138c24daab3032df791dd464a932d42
Alexander Popov <alex.wayfer@gmail.com>
2018-08-13 07:31:07 +0300

    Assign `cccombo` property with an instance to elements

--- cccombo.js
+++ cccombo.js
@@ -398,7 +398,7 @@ Cccombo.prototype = {
 document.addEventListener('DOMContentLoaded', function() {
 	var cccomboArray = document.getElementsByClassName('cccombo');
 	[].forEach.call(cccomboArray, function(element) {
-		var cccombo = new Cccombo(element);
-		cccombo.init();
+		element.cccombo = new Cccombo(element);
+		element.cccombo.init();
 	});
 });
